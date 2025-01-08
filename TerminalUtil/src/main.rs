35a6caa532ea4049ef66b062a4320bd6b2baa91a
jisef <josef.holzapfel63@gmail.com>
2025-01-08 10:40:39 +0100

    code cleanup TerminalUtil

--- TerminalUtil/src/main.rs
+++ TerminalUtil/src/main.rs
@@ -2,16 +2,78 @@ use std::collections::LinkedList;
 use std::env;
 use std::fs;
 
-fn get_filepath_through_shell() -> String {
-    let shell = get_shell();
-    let mut filepath = String::new();
+#[derive(Debug)]
+#[derive(Clone)]
+struct Command {
+    command: String,
+    count: u16
+}
 
-    if shell.contains("bash") {
-        String::from("/Users/josef/.bash_history")
-    } else {
-        String::from("/Users/josef/.zsh_history")
+
+impl PartialEq for Command {
+    fn eq(&self, other: &Command) -> bool {
+        self.command == other.command
+    }
+
+}
+struct Analyser {
+    list: LinkedList<Command>
+}
+
+impl Analyser {
+    fn analyse_line(&mut self, line: &str) {
+        let line = line.split(" ").next().unwrap();
+        if line.starts_with(":") {
+            return;
+        }
+        let command = Command {
+            command: String::from(line),
+            count: 1,
+        };
+        if !self.list.contains(&command) {
+
+            self.list.push_front(command);
+        }
+        else {
+            let iter = self.list.iter_mut();
+            for awd in iter {
+                if awd.command == command.command {
+                    awd.count += 1;
+                }
+            }
+        }
+    }
+
+    fn get_count(&mut self) -> u16 {
+        let iter = self.list.iter();
+        let mut count:u16 = 0;
+        for cmd in iter {
+            count += cmd.count;
+        }
+        count
+    }
+
+    fn most_user_commands(&self) -> LinkedList<Command> {
+        self.list.clone()
+    }
+
+    fn get_most_used_commands(&self) -> Command{
+        let ll: LinkedList<Command> = sort_linked_list(self.list.clone());
+        let mut biggest:Command = Command {
+            command: String::new(),
+            count: 0
+        };
+        for cmd in ll {
+            if biggest.count < cmd.count {
+                biggest = cmd
+            }
+        }
+        biggest
     }
 }
+
+
+
 fn main() {
     let file = read_file(get_filepath_through_shell());
     let file = file.lines();
@@ -39,86 +101,23 @@ fn read_file(filepath: String) -> String {
     content
 }
 
+fn get_filepath_through_shell() -> String {
+    let shell = get_shell();
+    let mut filepath = String::new();
+
+    if shell.contains("bash") {
+        String::from("/Users/josef/.bash_history")
+    } else {
+        String::from("/Users/josef/.zsh_history")
+    }
+}
+
 #[cfg(unix)]
 fn get_shell() -> String {
     let sigma = env::var("SHELL").ok();
     match sigma {
         Some(sigma) => { Some(sigma).unwrap()},
         _ =>  sigma.unwrap().as_str().to_string(),
-    }
-}
-
-#[derive(Debug)]
-#[derive(Clone)]
-struct Command {
-    command: String,
-    count: u16
-}
-
-
-impl PartialEq for Command {
-    fn eq(&self, other: &Command) -> bool {
-        self.command == other.command
-    }
-    
-}
-struct Analyser {
-    list: LinkedList<Command>
-}
-impl Analyser {
-    fn add_command(&mut self, cmd: &Command) -> bool {
-        false
-    }
-}
-impl Analyser {
-    fn analyse_line(&mut self, line: &str) {
-        let line = line.split(" ").next().unwrap();
-        if line.starts_with(":") { 
-            return;
-        }
-        let command = Command {
-            command: String::from(line),
-            count: 1,
-        };
-        if !self.list.contains(&command) { 
-            
-            self.list.push_front(command);
-        }
-        else {
-            let iter = self.list.iter_mut();
-            for awd in iter {
-                if awd.command == command.command {
-                    awd.count += 1;
-                }
-            }
-        }
-    }
-    
-    fn get_count(&mut self) -> u16 {
-        let iter = self.list.iter();
-        let mut count:u16 = 0;
-        for cmd in iter {
-            count += cmd.count;
-        }
-        count
-    }
-    
-    fn most_user_commands(&self) -> LinkedList<Command> {
-        self.list.clone()
-    }
-    
-    fn get_most_used_commands(&self) -> Command{
-        let ll: LinkedList<Command> = sort_linked_list(self.list.clone());
-        let mut biggest:Command = Command {
-            command: String::new(),
-            count: 0
-        };
-        for cmd in ll {
-            if biggest.count < cmd.count { 
-                biggest = cmd
-            }
-        }
-        biggest
     }
 }
 

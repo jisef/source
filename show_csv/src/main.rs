8adf038ce4ab403210493b68e6a0a465b4d4f005
jisef <josef.holzapfel63@gmail.com>
2025-01-28 11:43:47 +0100

    added crates for tables

--- show_csv/src/main.rs
+++ show_csv/src/main.rs
@@ -1,12 +1,12 @@
+use std::collections::HashMap;
 use std::fs;
-use std::path::PathBuf;
 use std::str::{Lines};
-use colored::Colorize;
+use colored::{Color, Colorize};
 use pad::{PadStr, Alignment};
 use clap::{Arg, Command};
+use tabled::Table;
 
 fn main() {
-
     let matches = Command::new("Show CSV")
         .version("1.0")
         .author("jisef of GH")
@@ -48,10 +48,12 @@ fn main() {
     let sigma = read_csv(String::from(path));
     let max_width_per_column = get_max_width_per_column(&sigma, seperator);
     let sigma = sigma.lines();
-    print_lines(max_width_per_column, sigma, seperator, 2);
+    print_lines_default(max_width_per_column, sigma.clone(), seperator, 2);
+    print_table_with_comfy_table(sigma);
+    let s = init_colors(69);
 }
 
-fn print_lines(max_width_per_column: Vec<usize>, lines: Lines, seperator: char, offset: usize) {
+fn print_lines_default(max_width_per_column: Vec<usize>, lines: Lines, seperator: char, offset: usize) {
     let mut ges_width: usize = max_width_per_column.iter().sum();
     ges_width += (offset + 1) * max_width_per_column.len();
     let mut is_first = true;
@@ -79,6 +81,45 @@ fn print_lines(max_width_per_column: Vec<usize>, lines: Lines, seperator: char, 
 
     }
 }
+fn print_table_with_tabled(lines: Lines) {
+    let lines : Vec<_> = lines.collect();
+
+    let table = Table::new(&lines).to_string();
+
+    println!("{}", table);
+}
+
+fn print_table_with_comfy_table(lines: Lines) {
+    let mut table = comfy_table::Table::new();
+    for line in lines {
+        table.add_row(line.split(',').collect::<Vec<&str>>());
+    }
+    println!("{}", table);
+}
+fn init_colors(max_index: usize) -> HashMap<u16, Color> {
+    let default_colors = vec![
+        Color::Blue,
+        Color::Green,
+        Color::Red,
+        Color::Cyan,
+        Color::Magenta,
+        Color::Yellow,
+    ];
+
+    let mut index_colors: HashMap<u16, Color> = HashMap::new();
+
+    for i in 0..max_index {
+        // Use modulo to cycle through the default colors
+        let color = &default_colors[i % default_colors.len()];
+
+        index_colors.insert(i as u16, *color);
+    }
+
+    index_colors
+}
+
+
+
 
 fn get_max_width_per_column(text: &String, seperator: char) -> Vec<usize>{
     let lines = text.lines();
